81ed2cd3894cd70bd6f36fde147300d72481dc7a
igrkirillov <40744103+igrkirillov@users.noreply.github.com>
2019-08-10 20:29:20 +0300

    Delete HStaticClassMethodTest2.java

--- src/ru/x5/HStaticClassMethodTest2.java
+++
@@ -1,7 +0,0 @@
-package ru.x5;
-
-public class HStaticClassMethodTest2 extends HStaticClassMethodTest1 {
-  /*public static String test() {
-    return HStaticClassMethodTest2.class.getSimpleName();
-  }*/
-}
